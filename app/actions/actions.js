9821b0d8244e95e8bfe1bc67ff4453614720e679
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-05-01 20:56:53 +0530

    feat: add chatbot interface and integrate AI response functionality

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -121,3 +121,38 @@ export async function fetchCategoryNews(category, page=1) {
 
     }
 }
+
+
+export async function Chatbot(query) {
+    try {
+        if (!process.env.NEXT_PUBLIC_GEMINI_API_KEY) {
+            throw new Error("API key is not defined in the environment variables.");
+        }
+        const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
+        const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+        const prompt = `
+    Answer the following question based on the latest news articles. 
+    Provide a concise and informative response, including relevant details and context.
+
+    Here is the question:
+    "${query}"
+  `;
+
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const answer = response.text();
+
+        return {
+            data: answer,
+            success:true
+        }
+
+    } catch (error) {
+        console.error("Error fetching hot topics:", error);
+        return {
+            data: [],
+            success:false,
+            error:error.message
+        }
+    }
+}
